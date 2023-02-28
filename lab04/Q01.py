6afaa0770422049dd16834179c5147088f21a245
tiffanyngg <89659097+tiffanyngg@users.noreply.github.com>
2023-02-27 21:39:10 -0500

    Q01

--- lab04/Q01.py
+++ lab04/Q01.py
@@ -6,21 +6,26 @@ import requests
 import html5lib
 import csv
 
-# 
+# scrapes through the imdb website 
 source = requests.get("https://www.imdb.com/list/ls055592025/").text
 soup = BeautifulSoup(source, "lxml")
 
-csv_file = open("movie_scape.csv", "w", newline="", encoding="utf-8")
+# creates the csv file movie_scape.csv
+csv_file = open("movie_scrape.csv", "w", newline="", encoding="utf-8")
 csv_writer = csv.writer(csv_file)
 
-for element in soup.find_all("h3", class_="lister-item-header"):
+# iterates through the 100 movies and extracts the title, release date, and genre of each movie
+for element in soup.find_all("div", class_="lister-item-content"):
     title = element.a.text
     release = element.find("span", class_="lister-item-year text-muted unbold").text
-    genreSection = soup.find("span", class_="genre").text
+    genreSection = element.find("span", class_="genre").text.strip()
     
     print(title)
     print(release)
     print(genreSection)
+    print()
+
+    # adds the information to movie_scrape.csv 
     csv_writer.writerow([title, release, genreSection])
     
 csv_file.close()
